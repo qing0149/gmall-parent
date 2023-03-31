1782cf62521ee2d1d0a84b3058dfd66574a6a7b8
“jingqing” <qing0149@163.com>
2023-04-01 01:03:24 +0800

    test

---
+++ gmall-service/service-product/src/main/java/com/atguigu/gmall/product/controller/testController.java
@@ -0,0 +1,11 @@
+package com.atguigu.gmall.product.controller;// 直接赋值粘贴，删除CSDN的权限转载中文
+
+/**
+ * @program: gmall-parent
+ * @description:
+ * @author: jq
+ * @create: 2023-04-01 01:00
+ **/
+public class testController {
+
+}
